26b6b9d191fa87de5035f1b37cc6f20ac824538e
Anton <jereeko5@gmail.com>
2021-12-10 21:50:19 +0400

    Update app.py

--- app.py
+++ app.py
@@ -19,24 +19,17 @@ BalanceUrl = "https://blockchain.info/q/addressbalance/"
 def main():
     Mnemonic = generate_mnemonic(language = LanguageInMnemonic, strength = 128)
     Seed = Bip39SeedGenerator(Mnemonic).Generate()
-
-#    print("||----------------------------------------------------------------------------------------------------||")
-#    print("Mnemonic: " + Mnemonic)
-
     CheckedWalletBTCBip84 = Bip84.FromSeed(Seed, Bip84Coins.BITCOIN)
     CheckedWalletBTCBip84Step1 = (CheckedWalletBTCBip84.Purpose().Coin().Account(0)).Change(Bip44Changes.CHAIN_EXT)
     CheckedWalletBTCBip84Step2 = CheckedWalletBTCBip84Step1.AddressIndex(0)
     CheckedAddressBTCBip84 = CheckedWalletBTCBip84Step2.PublicKey().ToAddress()
-#    print("Address:  " + CheckedAddressBTCBip84)
-#    print("Seed:     " + base64.b64encode(Seed).decode('utf-8')) 
     r = requests.get(BalanceUrl + CheckedAddressBTCBip84)
     Balance = int(r.text)/100000000
-#    print("Balance:  " + str(Balance) + " BTC")
     print("Balance: " + str(Balance) + " BTC | " + "Address: " + CheckedAddressBTCBip84 + " | " + "Mnemonic: " + Mnemonic)
     if(Balance != 0.0):
         writeto(str(Mnemonic), str(CheckedAddressBTCBip84), str(Balance))
-    time.sleep(3)
-
+    time.sleep(3)               #if time < 3 server give permanent ban by ip
+    
 if __name__ == "__main__":
     while True:
         main()
